71f92a2f5bc2f8e378cd3d393695c144920b636e
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-27 01:50:50 +0000

    Fix space under block with img display block

--- themes/modo/template-parts/home/hero.php
+++ themes/modo/template-parts/home/hero.php
@@ -1,7 +1,7 @@
 <section class="hero">
 	<img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
 	<div class="container">
-		<h1 class="hero__title">Modo<br /> Museum</h1>
+		<h1 class="hero__title"><span>Modo</span><span>Museum</span></h1>
 		<div class="hero__description">
 			<p>Step into a world of timeless stories, rare collections, and unforgettable experiences.
 			</p>
